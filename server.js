bb4ce490a4ba2ab6244d938e889b5d80e49099cf
limsopeak16 <sopheaklim70@gmail.com>
2024-11-23 11:20:44 +0700

    completed delete uer, update profile and get profile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes'); // Import user routes
 
 dotenv.config(); // Load environment variables
 
@@ -16,7 +17,8 @@ app.use(cors());
 connectDB();
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes); // Register auth routes
+app.use('/api/users', userRoutes); // Register user routes
 
 // Start the server
 const PORT = process.env.PORT || 5000;
