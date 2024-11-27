e05331b7a8f8f984921c958bc06719e0e33bbb42
limsopeak16 <sopheaklim70@gmail.com>
2024-11-27 16:04:53 +0700

    Ignore .env file

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use('/api/auth', authRoutes); // Register auth routes
 app.use('/api/users', userRoutes); // Register user routes
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
